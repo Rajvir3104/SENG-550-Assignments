0f67bc81e6a98476366b24792fe2f18495b20493
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-09-25 11:12:03 -0600

    Adding payments table

---
+++ Assignment1/Part5.sql
@@ -0,0 +1,23 @@
+-- Address table links with customers
+-- Customers table would have to be updated to include foreign key to address_id
+CREATE TABLE address(
+    address_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    street TEXT,
+    city TEXT,
+    province TEXT,
+    postal_code TEXT
+);
+
+-- Payments table. Links with customers, and orders
+-- Stores amount, payment type (credit card, PayPal, debit)
+-- Orders table would have to have new column "payment_id" that is foreign key referencing payment row
+CREATE TABLE payments(
+    payment_id SERIAL PRIMARY KEY,
+    order_id INT REFERENCES orders(order_id),
+    amount INT,
+    payment_type TEXT,
+    status TEXT,
+    created_at TIMESTAMP,
+    updated_at TIMESTAMP
+);
--- Part5.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE address(
-    address_id SERIAL PRIMARY KEY,
-    customer_id INT REFERENCES customers(customer_id),
-    street TEXT,
-    city TEXT,
-    province TEXT,
-    postal_code TEXT,
-);
