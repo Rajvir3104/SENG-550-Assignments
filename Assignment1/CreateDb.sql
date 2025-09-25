ff733618ce132f97e98be7c868750fc7b2204a41
Rajvir <rajvirz2004@gmail.com>
2025-09-25 10:44:23 -0600

    added table

--- Assignment1/CreateDb.sql
+++ Assignment1/CreateDb.sql
@@ -22,3 +22,12 @@ delivery_id SERIAL PRIMARY KEY
 ,delivery_date DATE
 ,STATUS TEXT
 );
+
+CREATE TABLE address(
+    address_id SERIAL PRIMARY KEY,
+    customer_id INT REFERENCES customers(customer_id),
+    street TEXT,
+    city TEXT,
+    province TEXT,
+    postal_code TEXT,
+);
