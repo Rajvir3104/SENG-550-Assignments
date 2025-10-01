bd01f467d8a242837fe313f42734b6360d1a7c89
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-01 11:40:57 -0600

    A2: Part2 - update_customer_city and update_product_price

--- Assignment2/Part2.py
+++ Assignment2/Part2.py
@@ -1,4 +1,5 @@
 import psycopg2
+from datetime import datetime
 
 # Constants
 db_connect = psycopg2.connect(
@@ -22,8 +23,7 @@ def create_tables():
     print("Tables created.")
 
 
-
-# add product 
+# add product
 def add_product(prod_id: int, prod_name: str, category: str, price, float):
     cursor = db_connect.cursor()
     query = f"INSERT INTO products (prod_id, prod_name, category,price) VALUES (%d, %s, %s, %f)"
@@ -39,7 +39,7 @@ def add_product(prod_id: int, prod_name: str, category: str, price, float):
 def add_customer(customer_id: int, name: str, email: str, city: str):
     cursor = db_connect.cursor()
     query = f"INSERT  INTO customers (customer_id, name, email, city) VALUES (%d, %s, %s, %s)"
-    cursor.execute(query, (customer_id, name, email. city))
+    cursor.execute(query, (customer_id, name, email, city))
     db_connect.commit()
     fetched_row = cursor.fetchone()
     if not fetched_row:
@@ -47,3 +47,72 @@ def add_customer(customer_id: int, name: str, email: str, city: str):
     new_id = int(fetched_row[0])
     cursor.close()
     return new_id
+
+
+# get customer info
+def get_customer_info(customer_id: int) -> tuple:
+    cursor = db_connect.cursor()
+    query = f"SELECT name, email, city FROM dim_customers WHERE customer_id={customer_id} AND is_current=TRUE"
+    cursor.execute(query)
+    fetched_row = cursor.fetchone()
+    if not fetched_row:
+        return (-1, -1, -1)
+
+    cursor.close()
+    return fetched_row
+
+
+def update_customer_city(customer_id: int, new_city: str) -> None:
+    cursor = db_connect.cursor()
+
+    # get name and email thats not changing
+    customer = get_customer_info(customer_id)
+
+    # update old row
+    update_old_row = f"UPDATE dim_customers SET expiry_date = '{datetime.now().astimezone()}', is_current=FALSE WHERE customer_id={customer_id} AND is_current=TRUE"
+    cursor.execute(update_old_row)
+
+    # insert new row with new city
+    update_city_query = f"INSERT INTO dim_customers (customer_id, name, email, city) VALUES (%d, %s, %s, %s)"
+    cursor.execute(update_city_query, (customer_id, customer[0], customer[1], new_city))
+
+    db_connect.commit()
+    cursor.close()
+    return
+
+
+def get_product(product_id: int) -> tuple:
+    cursor = db_connect.cursor()
+    query = f"SELECT name, category, price FROM dim_products WHERE product_id={product_id} AND is_current=TRUE"
+    cursor.execute(query)
+    fetched_row = cursor.fetchone()
+    if not fetched_row:
+        return (-1, -1, -1)
+
+    cursor.close()
+    return fetched_row
+
+
+def update_product_price(product_id: int, new_price: float) -> None:
+    cursor = db_connect.cursor()
+    # get info of product thats not changing
+    product_info = get_product(product_id)
+
+    # update old row
+    update_old_product_query = f"UPDATE dim_products SET expiry_date='{datetime.now().astimezone()}', is_current=FALSE WHERE product_id={product_id} AND is_current=TRUE"
+    cursor.execute(update_old_product_query)
+
+    # insert new row with new price
+    new_row_query = f"INSERT INTO dim_products (product_id, name, category, price) VALUES (%d, %s, %s, %d)"
+    cursor.execute(
+        new_row_query, (product_id, product_info[0], product_info[1], new_price)
+    )
+
+    db_connect.commit()
+    cursor.close()
+    return
+
+
+if __name__ == "__main__":
+    reset_tables(["dim_customers", "customers", "products", "fact_sales"])
+    create_tables()
