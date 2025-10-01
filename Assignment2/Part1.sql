8357dd2f8521754fac247430416a723b9baea1d0
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-01 12:37:46 -0600

    Updated orders to be fact table

--- Assignment2/Part1.sql
+++ Assignment2/Part1.sql
@@ -8,7 +8,7 @@ CREATE TABLE dim_customers (
     effective_date TIMESTAMP DEFAULT NOW(),      -- When this record became effective
     expiry_date TIMESTAMP,                  -- When this record expired (NULL if current)
     is_current BOOLEAN DEFAULT TRUE    -- Flag for current record
-)
+);
 
 CREATE TABLE dim_products (
     id SERIAL PRIMARY KEY,
@@ -20,16 +20,12 @@ CREATE TABLE dim_products (
     effective_date TIMESTAMP DEFAULT NOW(),
     expiry_date TIMESTAMP, 
     is_current BOOLEAN DEFAULT TRUE
-)
+);
 
-CREATE TABLE dim_orders (
+CREATE TABLE fact_orders (
     id SERIAL PRIMARY KEY,
-    product_id INT REFERENCES dim_orders(order_id),
-    customer_id INT REFERENCES dim_customers(customer_id),
-    order_date DATE,
-    amount REAL,
-    -- type 2 tracking columns
-    effective_date TIMESTAMP DEFAULT NOW(),
-    expiry_date TIMESTAMP, 
-    is_current BOOLEAN DEFAULT TRUE
-)
+    product_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    order_date DATE DEFAULT CURRENT_DATE,
+    amount REAL NOT NULL
+);
