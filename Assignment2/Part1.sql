7c4174f8a225e981bab9bc34f9328fb13e467e2c
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-01 11:37:36 -0600

    Added default timestamp for effective_date

--- Assignment2/Part1.sql
+++ Assignment2/Part1.sql
@@ -5,7 +5,7 @@ CREATE TABLE dim_customers (
     email TEXT,
     city TEXT,
 --- type 2 tracking columns
-    effective_date TIMESTAMP NOT NULL,      -- When this record became effective
+    effective_date TIMESTAMP DEFAULT NOW(),      -- When this record became effective
     expiry_date TIMESTAMP,                  -- When this record expired (NULL if current)
     is_current BOOLEAN DEFAULT TRUE    -- Flag for current record
 )
