cd998f1ebfb03019297d18f7d748d9a453da4d0b
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-07 15:36:06 -0600

    part 3 query 3

--- Assignment2/Part3/query3.sql
+++ Assignment2/Part3/query3.sql
@@ -1,7 +1,17 @@
 -- Total Price vs. Actual Amount Difference (4 marks)
 -- Write a query to calculate the sum of differences between listed product price (at the
 -- time of the order) and paid amount (price - amount)
-SELECT price, amount, o.
-FROM dim_products p
-RIGHT JOIN fact_orders o
-ON o.product_id = p.product_id AND o.order_date BETWEEN p.effective_date AND p.expiry_date;
+SELECT
+    o.id,
+    p.name,
+    SUM(p.price - o.amount) AS Difference
+FROM
+    fact_orders o
+    LEFT JOIN dim_products p
+      ON o.product_id = p.product_id
+      AND o.order_date >= p.effective_date
+      AND (p.expiry_date IS NULL OR o.order_date < p.expiry_date)
+GROUP BY
+    o.id,
+    p.name
+ORDER BY o.id;
