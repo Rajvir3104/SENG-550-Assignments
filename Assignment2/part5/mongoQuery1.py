debe80fb1bdf1ed4b9e8a6bf3ba1ca806a5d9c5f
Aryan Karadia <aryan.karadia@ucalgary.ca>
2025-10-21 11:25:10 -0600

    added comments to explain more

--- Assignment2/part5/mongoQuery1.py
+++ Assignment2/part5/mongoQuery1.py
@@ -6,21 +6,18 @@ db = client['sales_db']
 collection = db['orders_summary']
 
 # Number of cities each customer has lived in
-returned = collection.aggregate([
-    {
-        "$group": {
-            "_id": "$customer.name",
-            "city_set": {"$addToSet": "$customer.city"}
-        }
-    },
-    {
-        "$project": {
-            "name": "$_id",
-            "city_count": {"$size": "$city_set"},
-            "_id": 0
-        }
-    }
-])
+returned = collection.aggregate(
+    [
+        {
+            "$group": {
+                "_id": "$customer.name",  # group key
+                "city_set": {"$addToSet": "$customer.city"},  # collect unique cities,
+            }
+        },
+        # project only desired fields, name as id and count of unique cities. also remove _id field
+        {"$project": {"name": "$_id", "city_count": {"$size": "$city_set"}, "_id": 0}},
+    ]
+)
 
 print("Number of cities each customer has lived in:")
 results = list(returned)
--- Assignment2/part5/mongoQuery2.py
+++ Assignment2/part5/mongoQuery2.py
@@ -6,25 +6,20 @@ db = client['sales_db']
 collection = db['orders_summary']
 
 # Total amount sold per city
-returned = collection.aggregate([
-    {
-        "$group": {
-            "_id": "$customer.city",          # group by city
-            "total_amount_sold": {"$sum": "$amount"}  # sum the "amount" field
-        }
-    },
-    {
-        "$project": {
-            "city": "$_id",
-            "total_amount_sold": 1,
-            "_id": 0
-        }
-    }
-])
+returned = collection.aggregate(
+    [
+        {
+            "$group": {
+                "_id": "$customer.city",  # group by city
+                "total_amount_sold": {"$sum": "$amount"},  # sum the "amount" field
+            }
+        },
+        # project only desired fields, city as id and total amount sold. also remove _id field
+        {"$project": {"city": "$_id", "total_amount_sold": 1, "_id": 0}},
+    ]
+)
 
 print("Total amount sold per city:")
 results = list(returned)
 for doc in results:
     print(doc)
-
-
