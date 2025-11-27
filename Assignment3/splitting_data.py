aa323b726404f5bbfa7f5eeaba195c147949a940
Rajvir <rajvirz2004@gmail.com>
2025-11-26 18:11:27 -0700

    i dont know what happened but i have the file correctly now lol

--- Assignment3/splitting_data.py
+++ Assignment3/splitting_data.py
@@ -6,7 +6,7 @@ df = pd.read_csv("orders.csv")
 for dow in range(7):
     subset = df[df["order_dow"] == dow]
     
-    outdir = f"part1/data/{dow}/raw/{dow}"
+    outdir = f"part1/data/raw/{dow}"
     os.makedirs(outdir, exist_ok=True)
     
     subset.to_csv(f"{outdir}/orders_{dow}.csv", index=False)
