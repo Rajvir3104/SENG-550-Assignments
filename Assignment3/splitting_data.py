168b21e9c400adee0cceccc6f7cf5e9f2b6991ff
Rajvir <rajvirz2004@gmail.com>
2025-11-26 17:08:11 -0700

    Part0 finished

---
+++ Assignment3/splitting_data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import os
+
+df = pd.read_csv("orders.csv")
+
+for dow in range(7):
+    subset = df[df["order_dow"] == dow]
+    
+    outdir = f"part1/data/{dow}/raw/{dow}"
+    os.makedirs(outdir, exist_ok=True)
+    
+    subset.to_csv(f"{outdir}/orders_{dow}.csv", index=False)
